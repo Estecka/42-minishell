119148126086c9fadc38eaa113424eb2c1dcd024
abaur <abaur@student.42.fr>
2020-09-06 18:50:16 +0200

    Parser tests using Valgrind.

--- parser/test.sh
+++ parser/test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-make test && ./parser.out < .test/test_subjects;
+EXEC=./parser.out
+LOG=valgrind.log
+
+make test \
+&& valgrind \
+	--tool=memcheck \
+	--leak-check=full \
+	--leak-resolution=high \
+	--show-reachable=yes \
+	--log-file=$LOG \
+	$EXEC < .test/test_subjects \
+;
+
+grep -A1 "valgrind" ${LOG}|grep $EXEC;
