51984d010dc87aa6354ba64313b505e259bb22be
abaur <abaur@student.42.fr>
2020-10-09 11:51:38 +0200

    Exposed g_envarray.

--- envvar/envvar.c
+++ envvar/envvar.c
@@ -14,7 +14,7 @@
 #include "../dynarray/dynarray.h"
 #include "../libft/libft.h"
 
-static t_dynarray	g_envarray = {0};
+extern t_dynarray	g_envarray = {0};
 static char			***g_environ;
 
 extern char			**envvarinit(char **environ)
