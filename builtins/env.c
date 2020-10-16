6e1da587c36273f7697a333801091c0af5c0bf11
Helene HERIN <hherin@e1r3p20.42.fr>
2020-10-16 15:20:01 +0200

    bug in env resolved

--- builtins/env.c
+++ builtins/env.c
@@ -6,25 +6,50 @@
 /*   By: hherin <hherin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 11:19:10 by hherin            #+#    #+#             */
-/*   Updated: 2020/10/16 12:07:35 by hherin           ###   ########.fr       */
+/*   Updated: 2020/10/16 15:19:40 by hherin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
+static char	*get_name(char *s)
+{
+	int		i;
+	char	*ret;
+
+	i = 0;
+	ret = NULL;
+	while (s[i] && s[i] != '=')
+		i++;
+	if (s[i] == '=')
+	{
+		ret = malloc(sizeof(char) * i + 1);
+		ft_strlcpy(ret, s, i);
+	}
+	return (ret);
+}
+
 int		env_built(int argc, char **args)
 {
 	int		i;
-	char	**tmp;
+	char	**env;
+	char	*value;
+	char	*name;
+	
 
 	i = 0;
 	(void)argc;
-	tmp = ((char**)(g_envarray.content));
+	env = ((char**)(g_envarray.content));
 	(void)args;
-	while (tmp[i])
+	while (env[i])
 	{
-		ft_putstr(tmp[i]);
-		write(1, "\n", 1);
+		name = get_name(env[i]);
+		value = (name) ? get_env_var(name) : NULL;
+		if (value) 
+		{
+			ft_putstr(env[i]);
+			write(1, "\n", 1);
+		}
 		i++;
 	}	
 	return (errno);
