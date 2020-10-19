ef9d9f48750c97e62e6e5cf8ac029a07bbfb2611
Helene HERIN <hherin@e2r12p4.42.fr>
2020-10-19 13:47:40 +0200

    unset done

--- builtins/unset.c
+++ builtins/unset.c
@@ -6,27 +6,27 @@
 /*   By: hherin <hherin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 15:23:50 by hherin            #+#    #+#             */
-/*   Updated: 2020/10/19 13:30:42 by hherin           ###   ########.fr       */
+/*   Updated: 2020/10/19 13:45:49 by hherin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-int		valide_unset(char *args)
-{
-	int 
-}
-
 int		unset_built(int argc, char **args)
 {
 	size_t	i;
+	int		ret;
 	
 	i = 1;
 	(void)argc;
 	while (args[i])
 	{
-		if (!envclear(args[i]))
-			return (0);
+		if (!ft_isalpha(args[i][0]) && args[i][0] != '_')
+		{
+			print_error("bash: unset: `", "': not a valid identifier", args[i]);
+			return (1);
+		}
+		ret = envclear(args[i]);
 		i++;
 	}
 	return(0);
