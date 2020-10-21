90a94ba3fded8d36bbb4724e43fc43b862e19a58
abaur <abaur@student.42.fr>
2020-10-21 13:55:41 +0200

    postproc_arg fully parses arguments.

--- parser/variables.c
+++ parser/variables.c
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/06 11:50:00 by abaur             #+#    #+#             */
-/*   Updated: 2020/10/21 12:04:36 by abaur            ###   ########.fr       */
+/*   Updated: 2020/10/21 13:52:51 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 /*
 ** Parses the name of an environnement variable within an argument.
+** The special variable name $? is supported.
 ** @param char** cursor	A cursor to the '$' preceding the variable's name.
 ** 	This cursor will be moved to the first character following the name.
 ** 	Its value will be undefined in case of error.
@@ -25,6 +26,11 @@ static char		*get_var_name(const char **cursor)
 	t_dynarray	name;
 
 	++*cursor;
+	if (**cursor == '?')
+	{
+		(*cursor)++;
+		return (ft_strdup("?"));
+	}
 	if (!dyninit(&name, sizeof(char), 8, 1))
 		return (NULL);
 	while (ft_isalnum(**cursor) || **cursor == '_')
@@ -41,9 +47,10 @@ static char		*get_var_name(const char **cursor)
 
 /*
 ** Replaces a single variable's name with its value.
-** @param t_dynarray* narg	The stringbuilder that contains the newly formed argume
-** nt.
+** @param t_dynarray* narg	The stringbuilder that contains the newly formed arg
+** ument.
 ** @param char** cursor	A cursor to the '$' preceding the variable's name.
+** 	On success, it will point to the last character of the variable's name.
 ** @return
 ** 	true 	OK
 ** 	false	Error
@@ -56,6 +63,7 @@ static short	append_var(t_dynarray *narg, const char **cursor)
 	short	r;
 
 	varname = get_var_name(cursor);
+	(*cursor)--;
 	if (!varname)
 		return (0);
 	varvalue = get_env_var(varname);
@@ -73,25 +81,28 @@ static short	append_var(t_dynarray *narg, const char **cursor)
 extern char		*postproc_arg(const char *arg)
 {
 	t_dynarray	narg;
+	char		quote;
 
 	if (!dyninit(&narg, sizeof(char), ft_strlen(arg), 1))
 		return (NULL);
-	while(*arg)
+	quote = 0;
+	while(*arg && !errno)
 	{
-		if (*arg != '$')
-		{
-			if (*arg == '\\' && *(arg + 1))
-				arg++;
-			dynappend(&narg, arg);
-			arg++;
-		}
-		else if (!append_var(&narg, &arg))
-		{
-			free(narg.content);
-			return (NULL);
-		}
+		if (!quote && (*arg == '\'' || *arg == '\"'))
+			quote = *arg;
+		else if (quote && *arg == quote)
+			quote = 0;
+		else if (quote != '\'' && *arg == '$')
+			append_var(&narg, &arg);
+		else if (*arg == '\\' && (!quote || ft_strcontain("\"\\$", *(arg + 1))))
+			dynappend(&narg, &*++arg);
+		else 
+			dynappend(&narg, &*arg);
+		arg++;
 	}
-	return (narg.content);
+	if (errno)
+		free(narg.content);
+	return (char*)((!errno) * (long)narg.content);
 }
 
 void		postproc_args_all(char **args)
