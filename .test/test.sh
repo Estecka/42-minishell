cbe1ea28cbc1234972aa22c0cd869a40c510c87f
abaur <abaur@student.42.fr>
2020-10-26 10:54:13 +0100

    Tweaked the test script.Can now take multiple different inputs.

---
+++ .test/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd $(dirname $BASH_SOURCE);
+
+if [ "$#" != "0" ];
+then
+	TEST_LISTS=$@
+else
+	TEST_LISTS=(
+		echo
+		export
+	);
+fi
+
+
+
+make minishell -C ../ || exit $?;
+
+for t in ${TEST_LISTS[@]};
+do
+	echo "	$t :"
+	../minishell <${t} >${t}.out.42sh.log 2>${t}.err.42sh.log;
+	bash         <${t} >${t}.out.bash.log 2>${t}.err.bash.log;
+	diff ${t}.out.42sh.log ${t}.out.bash.log;
+done;
