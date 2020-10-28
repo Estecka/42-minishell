464903cc91275a3764de9c34b47d22dd24afa153
abaur <abaur@student.42.fr>
2020-10-28 15:32:02 +0100

    Fix invalid read on empty or open quotes.

--- parser/quotes.c
+++ parser/quotes.c
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 15:02:00 by abaur             #+#    #+#             */
-/*   Updated: 2020/10/21 14:00:19 by abaur            ###   ########.fr       */
+/*   Updated: 2020/10/28 15:29:49 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@
 ** @param t_dynarray* chars	The character array where to store the quoted strin
 ** g.
 ** @param const char** cursor	A pointer to the first quote of the string.
-** 	This cursor will point to the terminating quote on success.
+** On success, this cursor will point to the terminating quote, or the last quot
+** ed character if there is no terminating quote.
 ** @return bool
 ** 	true 	OK
 ** 	false	Error
@@ -29,15 +30,17 @@ short			append_single_quote(t_dynarray *chars, const char **cursor)
 {
 	if (!dynappend(chars, "\'"))
 		return (0);
+	if (!*((*cursor) + 1))
+		return (1);
 	(*cursor)++;
 	while (**cursor)
 	{
 		dynappend(chars, &**cursor);
-		(*cursor)++;
 		if (errno)
 			return (0);
 		if (**cursor == '\'')
 			break ;
+		(*cursor)++;
 	}
 	return (1);
 }
@@ -48,6 +51,8 @@ short			append_double_quote(t_dynarray *chars, const char **cursor)
 
 	if (!dynappend(chars, "\""))
 		return (0);
+	if (!*((*cursor) + 1))
+		return (1);
 	(*cursor)++;
 	while (**cursor)
 	{
@@ -58,11 +63,11 @@ short			append_double_quote(t_dynarray *chars, const char **cursor)
 		}
 		else
 			dynappend(chars, &**cursor);
-		(*cursor)++;
 		if (errno)
 			return (0);
 		if (!escaped && **cursor == '\"')
 			break ;
+		(*cursor)++;
 	}
 	return (1);
 }
