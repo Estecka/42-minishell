49cd9552a830cc4a26d7174ece5b2d1449111f40
hherin <hherin@student.42.fr>
2020-11-02 12:29:20 +0100

    exit done

--- builtins/exit.c
+++ builtins/exit.c
@@ -1,12 +1,12 @@
-/* ************************************************************************** */
+ /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
+/*   By: heleneherin <heleneherin@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 12:05:31 by hherin            #+#    #+#             */
-/*   Updated: 2020/10/27 15:13:44 by abaur            ###   ########.fr       */
+/*   Updated: 2020/11/02 12:02:13 by heleneherin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ static int	is_str_digit(char *s)
 				break ;
 			else
 			{
-				print_error("minishell: exit: ",
-					": numeric argument required\n", s);
+				print_error("bash: exit: ",
+					": numeric argument required", s);
 				exit(255);
 			}
 		}
@@ -38,31 +38,38 @@ static int	is_str_digit(char *s)
 	return (1);
 }
 
+static void	num_arg(char **args, long long nb)
+{
+	if ((ft_strlen(args[1]) > 19 && ft_isdigit(args[1][0])) ||
+	(ft_strlen(args[1]) > 20 && args[1][0] == '-'))
+	{
+		print_error("bash: ", ": numeric argument required", args[0]);
+		nb = 255;
+		clean_exit(nb);
+	}
+	else if ((args[1][0] == '-' && nb > 0) ||
+	(ft_isdigit(args[1][0]) && nb < 0))
+	{
+		print_error("bash: ", ": numeric argument required", args[0]);
+		nb = 255;
+		clean_exit(nb);
+	}
+}
+
 int			exit_built(int argc, char **args)
 {
 	long long	nb;
 
 	nb = g_prev_status;
-	if (argc > 1)
+	if (argc == 1)
+		clean_exit(nb);
+	else
 	{
 		is_str_digit(args[1]);
 		nb = ft_atoi_ll(args[1]);
-		if ((ft_strlen(args[1]) > 19 && ft_isdigit(args[1][0])) ||
-			(ft_strlen(args[1]) > 20 && args[1][0] == '-'))
-		{
-			print_error("minishell: exit: ", ": numeric argument required\n",
-				args[0]);
-			nb = 255;
-		}
-		else if ((args[1][0] == '-' && nb > 0) ||
-			(ft_isdigit(args[1][0]) && nb < 0))
-		{
-			print_error("minishell: exit: ", ": numeric argument required\n",
-				args[0]);
-			nb = 255;
-		}
+		num_arg(args, nb);
 	}
-	ft_putstr_fd("exit\n", 2);
-	clean_exit(nb);
+	if (argc > 2)
+		print_error("bash: ", ": too many arguments", args[0]);
 	return (nb);
 }
